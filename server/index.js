01b4e8c50778777f0f04530e58c6a045d52b4bc5
vishesh.srivastava <vishesh.srivastava@tothenew.com>
2021-01-19 19:35:16 +0530

    designed the chat component, added real time chat support

--- server/index.js
+++ server/index.js
@@ -2,15 +2,43 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
 const PORT = 5000;
 
 const router = require('./router');
 
 io.on('connection', (socket) => {
-    console.log('A user connected!!');
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        if(error) return callback(error);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` }); // to welcome the user
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined` }); // to let everybody else know that some user has joined
+
+        socket.join(user.room);
+
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+
+        callback({});
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+        callback({});
+    })
 
     socket.on('disconnect', () => {
-        console.log('User disconnected!!!');
+        const user = removeUser(socket.id);
+
+        if(user){
+            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left` });
+        }
     });
 });
 
